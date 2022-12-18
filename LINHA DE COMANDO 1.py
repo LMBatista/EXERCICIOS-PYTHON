2fe5d9e8ddc8dff652154771f172ea13916a2794
Lucas Marquese Batista <lucasmarquese02@gmail.com>
2022-12-18 08:14:56 -0300

    Update LINHA DE COMANDO 1.py

--- LINHA DE COMANDO 1.py
+++ LINHA DE COMANDO 1.py
@@ -2,5 +2,6 @@ Nome=input('Qual o seu Nome?')
 Idade=input('Qual sua Idade?')
 Peso=input('Qual o seu Peso?')
 Altura=input('Qual a sua altura?')
+Profissão=input('Qual a sua Profissão?')
 print(Nome,Peso,Altura,Idade)
 
