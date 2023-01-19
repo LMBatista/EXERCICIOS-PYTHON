1a6d6e6e91985e4d0b4a34df628a67e576a1c018
Lucas Marquese Batista <lucasmarquese02@gmail.com>
2023-01-19 19:39:57 -0300

    Add files via upload

---
+++ FORMATAÇÃO PYTHON.py
@@ -0,0 +1,20 @@
+hora=10
+minutos=20
+segundos=50
+# Saída de dados com separador
+print(str(hora)+':'+str(minutos)+':'+str(segundos))
+
+#Saída de dados com f()
+print(f'{hora}:{minutos}:{segundos}')
+
+#Saída de dados com format()
+print('{}:{}:{}'.format(hora,segundos,minutos))
+
+#Impresão de Substrings
+Imagem=1,2,3,4,5,6,7
+print(Imagem[3:6])
+
+#Impresão de Substrings inversamente
+Imagem=1,2,3,4,5,6,7
+print(Imagem[::-2])
+
