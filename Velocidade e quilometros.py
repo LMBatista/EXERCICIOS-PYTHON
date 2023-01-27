36144261b5c2029e8d61d7c1298d737853f568dd
Lucas Marquese Batista <lucasmarquese02@gmail.com>
2023-01-27 18:42:29 -0300

    Update Velocidade e quilometros.py

--- Velocidade e quilometros.py
+++ Velocidade e quilometros.py
@@ -1,10 +1,10 @@
 class veiculo:
-    def __init__(self,nome,velocidade_maxima,quilometros_percorridos_por_litro)
+    def __init__(self,nome,velocidade_maxima,quilometros_percorridos_por_litro):
         self.nome=nome
         self.velocidade_maxima=velocidade_maxima
         self.quilometros_percorridos_por_litro=quilometros_percorridos_por_litro
      
-    def tostr(self)
+    def tostr(self):
       print(f'nome={self.nome}')
       print(f'velocidade_maxima={self.velocidade_maxima}')
       print(f'quilometros_percorridos_por_litro={self.quilometros_percorridos_por_litro}')
